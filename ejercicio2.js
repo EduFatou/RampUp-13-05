5edf0e27cec412be3f6de7bd9ca4456e98ff4dba
EduFatou <edufatou@gmail.com>
2024-05-13 17:15:34 +0200

    done

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,14 +1,42 @@
 /* function numerosImpares(x){
-    
+
     let cincuenta = [x, ...x+=50];
     for (let i = 0; i < cincuenta.length; i++){
         return cincuenta.filter((numero) => numero % 2 === 1);
     }
-    return 
+    console.log(numerosImpares(x));
 }
+
+function array(){
+    return Array.from();
+
+}
+
+function numerosImpares(x){
+    let arr = Array.from(x);
+    for (let i = 0; i < )
+
+}
+Array.from({ length: 5 }, (v, i) => i);
+
+let cincuenta = x.push()
+
+
+function numerosImpares(numero){
+    let cincuenta = [];
+    array = Array.from(numero);
+    for (let i = 0; i < array.length; i++){
+        return cincuenta.push(i);
+    }
+}
+console.log(numerosImpares(3));
 */
 
 function numerosImpares(x){
-    let arr = [x];
-
+    let cincuenta = Array.from({ length: (x + 50)  }, (v, i) => i);
+    for (let i = 0; i < cincuenta.length; i++){
+        return cincuenta.filter((numero) => numero % 2 === 1);
+    }
 }
+
+console.log(numerosImpares(4));
